27f155e6d4fd898bb3fe073e85cb2455ceee92dd
vidyadhar001 <sanjay.vidyadhar@walchandsangli.ac.in>
2024-02-16 14:53:32 +0530

    Commit 1

---
+++ app/src/main/java/com/example/rr/splashActivity.java
@@ -0,0 +1,34 @@
+package com.example.rr;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.example.learn.R;
+
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ImageView giff = findViewById(R.id.giff);
+
+        Glide.with(this).load(R.drawable.roomradar).into(giff);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent iHome = new Intent(splashActivity.this, MainActivity.class);
+                startActivity(iHome);
+            }
+        },4000);
+
+    }
+}
