afaf35a6623b095a02a47859c00ea9acefb8f139
vidyadhar001 <sanjay.vidyadhar@walchandsangli.ac.in>
2024-02-17 06:30:57 +0530

    Added SignUp and Login Page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,15 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.learn"
     compileSdk = 34
+
+    buildFeatures{
+        viewBinding=true
+    }
 
     defaultConfig {
         applicationId = "com.example.learn"
@@ -34,6 +39,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
---
+++ app/src/main/java/com/example/rr/LoginScreen.java
@@ -0,0 +1,81 @@
+
+package com.example.rr;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.learn.R;
+import com.example.learn.databinding.ActivityLoginScreenBinding;
+import com.example.learn.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginScreen extends AppCompatActivity {
+
+    private ActivityLoginScreenBinding binding;
+    private FirebaseAuth auth;
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentusr = auth.getCurrentUser();
+//        if(currentusr!= null){
+//            startActivity(new Intent(LoginScreen.this,MainActivity.class));
+//            finish();
+//        }
+//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+
+
+        binding.login2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailLogin= binding.emailLogin.getText().toString();
+                String passwordLogin = binding.passwordLogin.getText().toString();
+                if(emailLogin.equals("") || passwordLogin.equals("") ){
+                    Toast.makeText(LoginScreen.this, "Please fill all details", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    auth.signInWithEmailAndPassword(emailLogin,passwordLogin)
+                            .addOnCompleteListener(LoginScreen.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        startActivity(new Intent(LoginScreen.this,MainActivity.class));
+                                        finish();
+                                    }
+                                    else{
+                                        Toast.makeText(LoginScreen.this, "Registration Failed!?"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+
+        binding.signUpLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginScreen.this,SignUpPage.class));
+                finish();
+            }
+        });
+
+    }
+}
