84d9dfff9c25c59b1340551c2654aa930780db92
manali0501 <141013988+manali0501@users.noreply.github.com>
2024-02-19 10:09:01 +0530

    Add files via upload
    Signed-off-by: manali0501 <141013988+manali0501@users.noreply.github.com>

--- app/src/main/java/com/example/rr/MainActivity.java
+++ app/src/main/java/com/example/rr/MainActivity.java
@@ -1,35 +1,14 @@
-package com.example.rr;
+package com.example.inputpage;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.learn.R;
-import com.example.learn.databinding.ActivityMainBinding;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        binding = ActivityMainBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
-
-
-
     }
-
-
-
-
 }
