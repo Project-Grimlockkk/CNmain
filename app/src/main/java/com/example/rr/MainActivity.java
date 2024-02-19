359874e3db1863c46311ac7c5d6b6199907b417e
vidyadhar001 <sanjay.vidyadhar@walchandsangli.ac.in>
2024-02-19 10:26:03 +0530

    Revert "Add files via upload"
    This reverts commit 84d9dfff9c25c59b1340551c2654aa930780db92.

--- app/src/main/java/com/example/rr/MainActivity.java
+++ app/src/main/java/com/example/rr/MainActivity.java
@@ -1,14 +1,35 @@
-package com.example.inputpage;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.rr;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.learn.R;
+import com.example.learn.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+
+
+
     }
+
+
+
+
 }
