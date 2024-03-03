145c11e926ab0bf911c127d27bdfa0da46c16bf0
vidyadhardinde <135449569+vidyadhardinde@users.noreply.github.com>
2024-03-03 13:41:12 +0530

    till recycler view

--- app/src/main/java/com/example/rr/HomeFragment.java
+++ app/src/main/java/com/example/rr/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,14 +13,28 @@ import android.view.ViewGroup;
 
 import com.example.learn.R;
 
+import java.util.ArrayList;
+
 
 public class HomeFragment extends Fragment {
 
+    ArrayList<RoomDetailsModel> arrDetails = new ArrayList<>();
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home1, container, false);
+        View rootView = inflater.inflate(R.layout.listingpage, container, false);
+
+        RecyclerView recycler = rootView.findViewById(R.id.recycler);
+        recycler.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        arrDetails.add(new RoomDetailsModel(R.drawable.bg, R.drawable.baseline_man_24, R.drawable.distance_to_travel_between_two_points_svgrepo_com, "Harishankar Apartment", " 700m", "2", "RS 2000"));
+        arrDetails.add(new RoomDetailsModel(R.drawable.bg, R.drawable.baseline_man_24, R.drawable.distance_to_travel_between_two_points_svgrepo_com, "Harishankar Apartment", " 700m", "2", "RS 2000"));
+        arrDetails.add(new RoomDetailsModel(R.drawable.bg, R.drawable.baseline_man_24, R.drawable.distance_to_travel_between_two_points_svgrepo_com, "Harishankar Apartment", " 700m", "2", "RS 2000"));
+
+        recyclerContactAdapter adapter = new recyclerContactAdapter(getActivity(), arrDetails);
+        recycler.setAdapter(adapter);
+
+        return rootView;
     }
 }
