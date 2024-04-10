80361cdfeb4166ffaeaf2443563ef0d9214af1dd
vidyadhardinde <135449569+vidyadhardinde@users.noreply.github.com>
2024-04-10 21:05:40 +0530

    ss

--- app/src/main/java/com/example/rr/HomeFragment.java
+++ app/src/main/java/com/example/rr/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.rr;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.net.Uri;
@@ -39,6 +40,7 @@ public class HomeFragment extends Fragment {
     ArrayList<RoomDetailsModel> arrDetails = new ArrayList<>();
 
     ValueEventListener valueEventListener;
+    private ProgressDialog loadingDialog;
 //    DatabaseReference databaseReference;
 
     DatabaseReference databaseReference;
@@ -49,6 +51,7 @@ public class HomeFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.home_fragment, container, false);
 
         Button addingPGButton = rootView.findViewById(R.id.addingPG);
+
 
         addingPGButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,28 +70,9 @@ public class HomeFragment extends Fragment {
 
         datalist = new ArrayList<>();
 
-//        Resources resources = getResources();
-//        Uri imageUri1 = Uri.parse(resources.getResourceName(R.drawable.pg_1));
-//        Uri imageUri2 = Uri.parse(resources.getResourceName(R.drawable.pg_2));
-//        Uri imageUri3 = Uri.parse(resources.getResourceName(R.drawable.pg_3));
-//        Uri imageUri4 = Uri.parse(resources.getResourceName(R.drawable.pg_4));
-//        Uri imageUri5 = Uri.parse(resources.getResourceName(R.drawable.pg_5));
-//        Uri imageUri6 = Uri.parse(resources.getResourceName(R.drawable.pg_5));
-
-//        arrDetails.add(new RoomDetailsModel(R.drawable.pg_1, "Shivdatt Apartment", " 400m", "4", "Rs. 1800/-","Boys"));
-//        arrDetails.add(new RoomDetailsModel(R.drawable.pg_2,  "Asher Villa", " 100m", "6", "Rs. 2500/-","Boys"));
-//        arrDetails.add(new RoomDetailsModel(R.drawable.pg_3,  "Silver Oak Apartment", " 700m", "7", "Rs. 2000/-","Boys"));
-//        arrDetails.add(new RoomDetailsModel(R.drawable.pg_4,"Utkarsh Sadan", " 400m", "6", "Rs. 0/-","Girls"));
-//        arrDetails.add(new RoomDetailsModel(R.drawable.pg_5,"Harishankar Apartment", " 700m", "6", "Rs. 2000/-","Boys"));
-//        arrDetails.add(new RoomDetailsModel(R.drawable.pg_6, "Shree Complex", " 1000m", "6", "Rs. 1800/-","Boys"));
 
         recyclerContactAdapter adapter = new recyclerContactAdapter(getActivity(), datalist);
         recyclerView1.setAdapter(adapter);
-//        arrDetails = new ArrayList<>();
-//        adapter= new recyclerContactAdapter(getContext(), arrDetails);
-//        recycler.setAdapter(adapter);
-
-//        RoomDetailsClass roomDetailsClass;
 
         databaseReference= FirebaseDatabase.getInstance().getReference("PGs");
 
@@ -104,27 +88,10 @@ public class HomeFragment extends Fragment {
                     String parentKey = snapshot.getKey();
                     Log.d("FirebaseKey", "Parent Key: " + parentKey);
                     Log.d("FirebaseKey", "Key: " + key);
-//                    String apDistance =itemSnapshot.child("distance").getValue(String.class);
-//                    String gender =itemSnapshot.child("gender").getValue(String.class);
-//                    String apRent =itemSnapshot.child("rentInr").getValue(String.class);
-//                    String apVacancy =itemSnapshot.child("Vaccancy").getValue(String.class);
-//                    String apPhoto =itemSnapshot.child("pgPhotos").getValue(String.class);
 
-//                    RoomDetailsClass roomDetailsClass = new RoomDetailsClass();
-//                    roomDetailsClass.setApName(apName);
-//                    roomDetailsClass.setDistance(apDistance);
-//                    roomDetailsClass.setGender(gender);
-//                    roomDetailsClass.setRentInr(apRent);
-//                    roomDetailsClass.setVaccancy(apVacancy);
-//                    roomDetailsClass.setPgPhotos(apPhoto);
-//                    roomDetailsClass.setApName(apName);
                     for(DataSnapshot childSnapshot: itemSnapshot.getChildren()){
                         String grandChildKey = childSnapshot.getKey();
                         Log.d("FirebaseKey", "Grandchild Key: " + grandChildKey);
-//                        roomDetailsClass= itemSnapshot.getValue(RoomDetailsClass.class);
-//                        roomDetailsClass.setApName(apName);
-//                        roomDetailsClass.setRentInr(rentInr);
-//                        datalist.add(roomDetailsClass);
                     }
                     RoomDetailsClass roomDetailsClass= itemSnapshot.getValue(RoomDetailsClass.class);
                     datalist.add(roomDetailsClass);
@@ -138,24 +105,7 @@ public class HomeFragment extends Fragment {
             }
         });
 
-
-//        databaseReference.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-////                arrDetails.clear();
-//                for(DataSnapshot dataSnapshot:snapshot.getChildren()){
-//                    RoomDetailsModel dataClass = dataSnapshot.getValue(RoomDetailsModel.class);
-//                    arrDetails.add(dataClass);
-//                }
-//
-//                adapter.notifyDataSetChanged();
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//
-//            }
-//        });
+        
 
         return rootView;
     }
